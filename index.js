8a6b41916d67d4fa0722473f57da2890fa377057
Zuzureglement <39495203+Zuzureglement@users.noreply.github.com>
2024-04-19 15:12:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var status = 1;  // 1 - online, 7 - invisible
 
 
 user = new steamUser();
-user.logOn({"accountName": blitzclack, "password": Asoren095! , "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
+user.logOn({"accountName": ack, "password": A! , "twoFactorCode": steamTotp.generateAuthCode(shared_secret)});
 user.on('loggedOn', () => {
 	if (user.steamID != null) console.log(user.steamID + ' - Successfully logged on');
 	user.setPersona(status);               
