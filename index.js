906155c1091f1f28d4b5910bd245c6079bdc5488
Zuzureglement <39495203+Zuzureglement@users.noreply.github.com>
2024-04-19 15:05:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
+var games = [730, 440, 570, 377160, 377160, 393380, 553850, 1245620, 1593500, 582010];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
